fbbbdc193692f712c8981b5b9f0ae03d554f2711
Your Name <335908665@qq.com name = XCup>
2018-08-21 15:47:07 +0800

    1547

--- formsubmit.py
+++ formsubmit.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 class indexHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
         print("setting headers!!!")
-        self.set_header("Access-Control-Allow-Origin", "*") # 这个地方可以写域名
+        self.set_header("Access-Control-Allow-Origin", "*") # 这个地方可以写域名123  
         self.set_header("Access-Control-Allow-Headers", "x-requested-with")
         self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
     def get(self, *args, **kwargs):

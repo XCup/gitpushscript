86a9ecb14b17ea0fbe6e34b1ffaacf9d95fe9350
XCup <335908665@qq.com>
2018-08-25 21:31:45 +0800

    inputNote

--- login.py
+++ login.py
@@ -2,29 +2,73 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
+from asn1crypto._ffi import null
+import simplejson as json
+
+import mysqlconnectorpython
 import re
 from tornado.options import define,options
 
-class loginregisterHandler(tornado.web.RequestHandler):
-    def get(self, *args, **kwargs):
-        self.write()
-    def post(self, *args, **kwargs):
+# class loginregisterHandler(tornado.web.RequestHandler):
+#     def get(self, *args, **kwargs):
+#         self.write()
+#     def post(self, *args, **kwargs):
 
 class loginHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        print("setting headers!!!")
+        self.set_header("Access-Control-Allow-Origin", "*") # 这个地方可以写域名123
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
     def get(self, *args, **kwargs):
         self.write()
     def post(self, *args, **kwargs):
+        username = self.get_argument('username')
+        passwd = self.get_argument('passwd')
+        mctp = mysqlconnectorpython
+        loginresults=mctp.selectsql("select username from users where username='"+username+"' AND passwd='"+passwd+"'")
+        self.set_header('Content-Type', 'application/json; charset=UTF-8')
+        if(loginresults!=None):
+            if(loginresults[0]!=""):
+                self.write(json.dumps({'type': 0}))
+                print("====>0:")
+            else:
+                self.write(json.dumps({'type': 1}))
+                print("====>1:")
+        else:
+            self.write(json.dumps({'type': 1}))
+            print("====>2:")
+        self.finish()
 
 class registerHandler(tornado.web.RequestHandler):
+    def set_default_headers(self):
+        print("setting headers!!!")
+        self.set_header("Access-Control-Allow-Origin", "*") # 这个地方可以写域名123
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
     def get(self, *args, **kwargs):
         self.write()
     def post(self, *args, **kwargs):
+        self.set_header('Content-Type', 'application/json; charset=UTF-8')
+        username = self.get_argument('username')
+        passwd = self.get_argument('passwd1')
+        mctp = mysqlconnectorpython
+        userresults = mctp.selectsql("select username from users where username='"+username+"'")
+        print(userresults)
+        if (userresults != None):
+            self.write(json.dumps({'type': 1}))
+            print("====>1")
+        else:
+            mctp.insertsql("insert into Users(username,passwd,lv) values('"+username+"','"+passwd+"',1);")
+            self.write(json.dumps({'type': 0}))
+            self.finish()
 
 if __name__=='__main__':
     app = tornado.web.Application([
-        ('/',loginregisterHandler)
-        ('/login',loginHandler)
+        #('/',loginregisterHandler)
+        ('/login',loginHandler),
         ('/register',registerHandler)
     ])
+
     app.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
--- mysqlconnectorpython.py
+++ mysqlconnectorpython.py
@@ -1,44 +1,46 @@
-class mysqlConPython ():
-    import pymysql
-    db = pymysql.connect('localhost','root','123456','gitdb')
+import pymysql
+
+#class mysqlConPython ():
+# def connectmysql(self):
+#
+#     #cursor.execute('SELECT VERSION()') #查询数据库信息
+#     #data = cursor.fetchone()
+#     #print("Database version: %s" % data)
+#     return db
+
+def selectsql(sql):
+    db = pymysql.connect('localhost', 'root', '123456', 'gitdb')
     cursor = db.cursor()
-    # cursor.execute('SELECT VERSION()') #查询数据库信息
-    # data = cursor.fetchone()
-    # print("Database version: %s" % data)
-    # db.close()
-
-    # sql = "INSERT INTO Users(username,passwd,lv) VALUES ('%s','%s','%d')" % ('XCup','123456',10) # 增加数据
-    #
-    # try:
-    #     cursor.execute(sql)
-    #     db.commit()
-    # except:
-    #     db.rollback()
-    #     print("error:mother fucker")
-    # db.close()
-
-    sql = "SELECT * FROM Users" #查询数据
+    print(sql)
     try:
         cursor.execute(sql)
         results = cursor.fetchall()
-        for row in results:
-            username = row[0]
-            passwd = row[1]
-            lv = row[2]
-            print("username=%s,passwd=%s,lv=%d" % (username,passwd,lv))
+        return results
     except:
         print("error:unable to fetch data")
     db.close()
 
-    # sql = "DELETE FROM Users" #删除数据
-    # try:
-    #     cursor.execute(sql)
-    #     db.commit()
-    # except:
-    #     db.rollback()
-    # db.close()
+def insertsql(sql):
+    db = pymysql.connect('localhost', 'root', '123456', 'gitdb')
+    cursor = db.cursor()
+    try:
+        cursor.execute(sql)
+        db.commit()
+    except:
+        db.rollback()
+        print("error:mother fucker")
+    db.close()
+
+# sql = "DELETE FROM Users" #删除数据
+# try:
+#     cursor.execute(sql)
+#     db.commit()
+# except:
+#     db.rollback()
+# db.close()
 
 # import mysql.connector #廖雪峰 mysql python 链接
 # conn=mysql.connector.connect(user='root',password='123456',database='gitdb')
 # cursor = conn.cursor()
 # cursor.execute('insert into users (username,passwd,lv) values (%s,%s,%d)',['XCup','123456',10])
+# "INSERT INTO Users(username,passwd,lv) VALUES ('%s','%s','%d')" % ('XCup','123456',10) # 增加数据
